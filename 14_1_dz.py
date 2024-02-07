73340ebd8c38669551cf04d974314bb9ec4687dc
Nurdaulet <134767660+Nurdauletqwe@users.noreply.github.com>
2024-02-07 21:49:26 +0600

    Add files via upload

---
+++ 14_1_dz.py
@@ -0,0 +1,55 @@
+'''1'''
+# class Car:
+#     def __init__(self, model, data, author, engine, color, price):
+#         self.model = model
+#         self.data = data
+#         self.author = author
+#         self.engine = engine
+#         self.color = color
+#         self.price = price
+    
+#     def get_code(self):
+#         return self.__code
+    
+#     def set_code(self, code):
+#         self.__code = code
+
+        
+# car = Car('Toyota Camry, ', '1999, ', 'Human, ', '2487 cc, ', 'black, ', 15000000)
+# print(car.model, car.data, car.author, car.engine, car.color, car.price)
+'''2'''
+# class Book:
+#     def __init__(self, name, data, company, genre, author, price):
+#         self.name = name
+#         self.data = data
+#         self.author = author
+#         self.company = company
+#         self.genre = genre
+#         self.price = price
+    
+#     def get_code(self):
+#         return self.__code
+    
+#     def set_code(self, code):
+#         self.__code = code
+
+# book = Book('Harry Potter, ', '1999, ', 'Disney, ', 'comedy, ', 'Human, ', 20000)
+# print(book.name, book.data, book.company, book.genre, book.author, book.price)
+'''3'''  
+# class Stadium:
+#     def __init__(self, name, data, country, city, capacity):
+#         self.name = name
+#         self.data = data
+#         self.country = country
+#         self.city = city
+#         self.capacity = capacity
+    
+#     def get_code(self):
+#         return self.__code
+    
+#     def set_code(self, code):
+#         self.__code = code
+
+        
+# stadium = Stadium('Camp Nou, ', '1899, ', 'Spain, ', 'Barcelona, ', 80000)
+# print(stadium.name, stadium.data, stadium.country, stadium.city, stadium.capacity)
